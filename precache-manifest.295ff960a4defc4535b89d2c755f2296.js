8b22baacbdcc806fe92fd5b0ff77785fba15d597
hiijs <bernadetta.nycz@icloud.com>
2019-04-10 16:42:05 +0200

    Updates

---
+++ precache-manifest.295ff960a4defc4535b89d2c755f2296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-travels/./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59cad12d909d6c741126",
+    "url": "/my-travels/./static/js/runtime~main.68e6477a.js"
+  },
+  {
+    "revision": "d13886530918bacfea22",
+    "url": "/my-travels/./static/js/main.4f9256c4.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/my-travels/./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d13886530918bacfea22",
+    "url": "/my-travels/./static/css/main.cbb4cc85.chunk.css"
+  },
+  {
+    "revision": "98b85bf63ec34de8a1565177e58e51c8",
+    "url": "/my-travels/./index.html"
+  }
+];
